c45d4e8b83d64dbc1739e3eb436dc70c42f66d20
Petr Spac <petrik.spac@gmail.com>
2023-10-05 23:49:01 +0200

    feat: match count and logic

--- keyfilter.c
+++ keyfilter.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <ctype.h>
 #include <stdbool.h>
 #include <stdio.h>
@@ -13,62 +12,80 @@ bool isDuplicit(char letter, char letters[], int letters_count);
 int main(int argc, char *argv[]) {
 
   char user_search[MAX_CHAR];
-  // char *addresses[MAX_ADDRESSES];
-  char letters[MAX_CHAR];
+  int user_search_length = 0;
+  char enabled_letters[MAX_CHAR];
+  int enable_letters_count = 0;
+  int matches = 0;
+  char last_match[MAX_CHAR];
 
   if (argc == 2) {
     // TODO: Maybe some input validation?
     strcpy(user_search, argv[1]);
-    printf("User input %s %lu", user_search, strlen(argv[1]));
+    user_search_length = strlen(argv[1]);
+    printf("User input %s %lu\n", user_search, strlen(argv[1]));
   }
 
   char buf[MAX_CHAR]; // 99 charachters + eol per line
-  int word_count = 0;
-  int used_letter_count = 0;
   while (fgets(buf, sizeof buf, stdin) != NULL) {
 
-    // Loop throught word
     printf("word: %s", buf);
-    for (int i = 0; buf[i] != '\n'; i++) {
-      printf("%c %i\n", buf[i], i);
+    // No argument, we care only about first letters
+    if (argc != 2) {
+      // Chech for duplicity
+      if (!isDuplicit(buf[0], enabled_letters, enable_letters_count)) {
+        enabled_letters[enable_letters_count] = buf[0];
+        enable_letters_count += 1;
+        printf("letter saved: %d\n", enable_letters_count);
+      }
+    } else if (argc == 2) {
 
-      buf[i] = toupper(buf[i]); // We do not care abou size
+      int equal_letters = 0;
+      for (int i = 0; i < user_search_length; i++) {
+        user_search[i] = toupper(user_search[i]);
+        buf[i] = toupper(buf[i]);
+        printf("Comparing %c, %c equal: %d\n", user_search[i], buf[i],
+               equal_letters);
 
-      // We do not have any input
-      if (argc != 2) {
-        // Chech for duplicity
-        if (!isDuplicit(toupper(buf[i]), letters, used_letter_count)) {
-          letters[used_letter_count] = buf[i];
-          // letters[used_letter_count + 1] = '\0'; MAYBE???
-          used_letter_count += 1;
-          printf("letter saved: %c %d\n", letters[word_count],
-                 used_letter_count);
-        }
-        break;
-      } else if (argc == 2) {
-
-        if (buf[i] != user_search[i]) {
-          printf("not found %c\n", user_search[i]);
-          letters[used_letter_count] = buf[i];
-          used_letter_count += 1;
+        if (user_search[i] == buf[i]) {
+          printf("Equals!!\n");
+          equal_letters += 1;
+        } else {
           break;
         }
       }
+
+      printf("IS MATCH? equal_letters %d, search_length %d\n", equal_letters,
+             user_search_length);
+      if (equal_letters == user_search_length) {
+        enabled_letters[enable_letters_count] = buf[user_search_length];
+        enable_letters_count += 1;
+        matches += 1;
+        strcpy(last_match, buf);
+      }
     }
-    // if argv[1] letter after that
-    // else save first letter
-    word_count += 1;
   }
 
-  // Sort all letters here
-  printf("word count %d letter: %d\n", word_count, used_letter_count);
-  for (int j = 0; j < used_letter_count; j++) {
-    printf("Letter: %c\n", letters[j]);
+  printf("STATUS: matches: %d\n", matches);
+  if (matches == 1) {
+    printf("Found: ");
+    // print here final word
+  } else if (matches > 1) {
+    printf("Enable: ");
+    // Sort all letters here
+    for (int j = 0; j < enable_letters_count; j++) {
+      printf("%c", enabled_letters[j]);
+    }
+    printf("\n");
+
+  } else if (matches == 0) {
+    printf("Not found\n");
+    return 0;
   }
 
   return 0;
 }
 
+// DO not use bool
 bool isDuplicit(char letter, char letters[], int letters_count) {
   for (int i = 0; i < letters_count; i++) {
     // printf("check duplicity: %c, %c\n", letter, letters[i]);
